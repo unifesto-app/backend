57b13d130b63a0b86d8d45ed88391b226380dcf8
Abhinavtej Reddy <abhinavtej.7a@gmail.com>
2026-06-06 17:00:03 +0530

    fix: add AuthModule to AwsModule

--- src/aws/aws.module.ts
+++ src/aws/aws.module.ts
@@ -4,9 +4,10 @@ import { AwsService } from './aws.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { RedisModule } from '../redis/redis.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, RedisModule, ConfigModule],
+  imports: [PrismaModule, RedisModule, ConfigModule, AuthModule],
   controllers: [AwsController],
   providers: [AwsService],
   exports: [AwsService],
