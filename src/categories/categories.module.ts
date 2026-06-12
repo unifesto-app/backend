18033da571f95528d04d073ade9c38e2d981d050
Abhinavtej Reddy <abhinavtej.7a@gmail.com>
2026-06-12 18:51:47 +0530

    fix: add AuthModule to CategoriesModule

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [CategoriesController],
   providers: [CategoriesService],
   exports: [CategoriesService],
