62201569e7949af2ed5b61c03f362c7a707dc01e
Abhinavtej Reddy <abhinavtej.7a@gmail.com>
2026-06-28 19:03:14 +0530

    fix: add AuthModule to PayoutsModule

--- src/payouts/payouts.module.ts
+++ src/payouts/payouts.module.ts
@@ -3,10 +3,11 @@ import { PayoutsController } from './payouts.controller';
 import { PayoutsService } from './payouts.service';
 import { PayoutsSchedulerService } from './payouts-scheduler.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 import { EmailModule } from '../email/email.module';
 
 @Module({
-  imports: [PrismaModule, EmailModule],
+  imports: [PrismaModule, EmailModule, AuthModule],
   controllers: [PayoutsController],
   providers: [PayoutsService, PayoutsSchedulerService],
   exports: [PayoutsService],
