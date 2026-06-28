1f2e01776b34a0bdad81273d9c70fd0e987c1c52
Abhinavtej Reddy <abhinavtej.7a@gmail.com>
2026-06-28 19:09:05 +0530

    fix: add AuthModule to PayoutsModule

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Unifesto!';
+    return '© 2026 Unifesto Private Limited. All rights reserved.';
   }
 }
